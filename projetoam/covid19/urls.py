ba685b121c161037163f11cd7c243d904eb4b1d3
Daniel Herbert Barbosa Junior <66037869+DanielBarbosabit@users.noreply.github.com>
2022-11-21 08:55:25 -0300

    Update urls.py

--- projetoam/covid19/urls.py
+++ projetoam/covid19/urls.py
@@ -4,12 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index', views.index, name='index'),
-    path('indextest', views.index, name='index'),
-    path('indexstart', views.index, name='index'),
-    path('ind', views.index, name='index'),
-    path('inde', views.index, name='index'),
-    path('in', views.index, name='index'),
-    path('newpath', views.index, name='index'),
-    path('path', views.index, name='index'),
 ]
